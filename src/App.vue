38de8a50d240501a94264765246dd25ba68ed8b9
Oleh Karaulnyi <olegkaraaa@gmail.com>
2022-10-05 14:43:03 +0300

    init + added base styles

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import AppButton from "./components/AppButton.vue";
+</script>
+
+<template>
+  <main>
+    <div class="advice-wrap">
+      <div class="advice-num">ADVICE #220</div>
+      <div class="advice-text">
+        “Most things are not as bad as you think they are.”
+      </div>
+      <AppButton text="Btn" />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.advice-wrap {
+  background-color: #323a49;
+  color: #cee3e9;
+
+  width: 100%;
+  max-width: 500px;
+  margin-bottom: 50px;
+  padding: 50px 10px 0;
+  border-radius: 15px;
+  text-align: center;
+  box-shadow: 0 10px 25px rgb(0 0 0 / 30%);
+  color: hsl(193, 38%, 86%);
+}
+
+.advice-num {
+  margin-top: 0;
+  margin-bottom: 30px;
+  text-transform: uppercase;
+  letter-spacing: 0.3em;
+  font-size: 12px;
+  color: hsl(150, 100%, 66%);
+}
+
+.advice-text {
+  padding-bottom: 30px;
+
+  font-size: 28px;
+
+  border-bottom: 1px solid hsla(193, 38%, 86%, 0.363);
+}
+</style>
---
+++ src/components/AppButton.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <button class="btn">{{ text }}</button>
+</template>
+
+<style scoped>
+.btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 60px;
+  height: 60px;
+  border: none;
+  margin: 0 auto;
+  border-radius: 100%;
+  transform: translateY(50%);
+  background-color: hsl(150, 100%, 66%);
+  cursor: pointer;
+  transition: box-shadow 250ms ease;
+}
+
+.btn:hover {
+  box-shadow: 0 0 30px hsl(150deg 100% 66%);
+}
+</style>
